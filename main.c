c78ed11483bb351f80ee121826c25c052dc66d66
ErickIssa <erickvinicius19675@gmail.com>
2026-05-07 20:26:35 -0300

    preparando main

--- main.c
+++ main.c
@@ -3,30 +3,8 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-int main(void)
-{
-    int i;
-    pid_t pid;
-
-    if ((pid = fork()) < 0)
-    {
-        perror("fork");
-        exit(1);
-    }
-    if (pid == 0)
-    {
-        sleep(2);
-        //O código aqui dentro será executado no processo filho
-        printf("pid do Filho: %d\n", getpid());
-    }
-    else
-    {
-        //O código neste trecho será executado no processo pai
-        printf("pid do Pai: %d\n", getpid());
-    }
+int main(void) {
 
 
-    printf("Esta regiao sera executada por ambos processos\n\n");
-    scanf("%d", &i);
-    exit(0);
+   return 0;
 }
