22e8fca133ceb11b6477cd04211f028ea52f483a
Júlio César <julio.c.oliveira@ufv.br>
2026-05-22 21:42:15 -0300

    atulizando processo simulado
    Removed commented-out code for process state saving and initialization.

--- src/ProcessoSimulado.c
+++ src/ProcessoSimulado.c
@@ -4,42 +4,6 @@
 #include "../include/ProcessoSimulado.h"
 #include "../include/Globais.h"
 
-// void salvaEstado(processo* proc, int pcAtual, int* variaveisAtuais, int quantumAtual, int nVar) {
-
-//     proc->pcCounter = pcAtual;
-//     proc->quantum=quantumAtual;
-
-//     for (int i = 0; i < nVar; i++) {
-//         proc->variaveis[i] = variaveisAtuais[i];
-//     }
-// }
-
-// void inicializarProcesso(processo* proc, int id, int nVar, int nInstrucao, int* variaveisIniciais, enum estado estadoInicial, int prioridade, instrucao* listaInstrucoes){
-
-//     proc->pid=proximoPidDisponivel;
-//     proc->nVariaveis=nVar;
-//     proc->pcCounter=0;
-//     proc->estado=estadoInicial;
-//     proc->quantum=0;
-//     proc->quantum_usado_CPUatual=0;
-//     proc->prioridade=prioridade;
-//     proc->tempoBloqueado=0;
-//     proc->nInstrucoes=nInstrucao;
-
-//     proc->variaveis = (int*) malloc(sizeof(int) * nVar);
-//     if (proc->variaveis != NULL) {
-//         for (int i = 0; i < nVar; i++) {
-//             proc->variaveis[i] = variaveisIniciais[i];
-//         }
-//     }
-
-//     proc->listaInstrucoes = (instrucao*) malloc(sizeof(instrucao) * nInstrucao);
-//     if (proc->listaInstrucoes != NULL) {
-//         for (int i = 0; i < nInstrucao; i++) {
-//             proc->listaInstrucoes[i] = listaInstrucoes[i];
-//         }
-//     }
-// }
 
 void inicializarProcessoInit(processo* proc, instrucao* listaInstrucoes, int nInstrucao){
 
@@ -93,7 +57,6 @@ void inicializarProcesso(processo* proc, instrucao* listaInstrucoes, int nInstru
     proc->pcCounter=0;
     proc->estado=PRONTO;
 
-    //proc->quantum=15;
     proc->quantum_usado_CPUatual=0;
 
     proc->tempoBloqueado=0;
@@ -242,54 +205,4 @@ void imprimirProcesso(processo *p){
         printf("Nenhuma variavel alocada.\n");
     }
 
-//     printf("\n----- Instrucoes -----\n");
-
-//     printf("Numero de instrucoes: %d\n",
-//            p->nInstrucoes);
-
-//     if(p->listaInstrucoes != NULL){
-
-//         for(int i = 0; i < p->nInstrucoes; i++){
-
-//             instrucao inst = p->listaInstrucoes[i];
-
-//             printf("[%d] Tipo: %c",
-//                    i,
-//                    inst.tipo);
-
-//             if(inst.tipo == 'N'){
-//                 printf(" n=%d", inst.n);
-//             }
-
-//             else if(inst.tipo == 'D'){
-//                 printf(" x=%d", inst.x);
-//             }
-
-//             else if(inst.tipo == 'V' ||
-//                     inst.tipo == 'A' ||
-//                     inst.tipo == 'S'){
-
-//                 printf(" x=%d n=%d",
-//                        inst.x,
-//                        inst.n);
-//             }
-
-//             else if(inst.tipo == 'R'){
-
-//                 printf(" arquivo=%s",
-//                        inst.caminhoArquivo);
-//             }
-//             else if(inst.tipo == 'F'){
-//                 printf(" %d", inst.n);
-//             }
-
-//             printf("\n");
-//         }
-//     }
-//     else{
-//         printf("Nenhuma instrucao carregada.\n");
-//     }
-
-//     printf("================================\n");
 }
-
